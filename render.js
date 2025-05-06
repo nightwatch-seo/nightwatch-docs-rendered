ab55499761b3fe0bc49fe478fa3442c3cf302914
Introvert <aljaz.fajmut@gmail.com>
2025-05-06 11:31:10 +0400

    disable chr sandbox

--- render.js
+++ render.js
@@ -10,7 +10,10 @@ if (!TARGET_URL) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({ 
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
   await page.goto(TARGET_URL, { waitUntil: 'networkidle0' });
